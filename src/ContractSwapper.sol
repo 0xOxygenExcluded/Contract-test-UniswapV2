a5598efa3d475ca4d88ae3fa710b1b4fcb364ff5
0xOxygenExcluded <oxygenexcluded@gmail.com>
2024-10-15 11:24:53 +0300

    refactor: delete useless requirements, owner can send ETH and Tokens to choosed address

--- src/ContractSwapper.sol
+++ src/ContractSwapper.sol
@@ -5,12 +5,13 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {console} from "forge-std/Test.sol";
 
 
 interface IUniswapV2Router02 {
     function swapExactETHForTokens(
         uint amountOutMin,
-        address[] calldata path, // массив чего обмениваем на что eth -> tokens
+        address[] calldata path, // массив пар чего обмениваем на что eth -> tokens
         address to,
         uint deadline
         ) external payable returns (uint[] memory amounts);
@@ -31,6 +32,7 @@ contract ContractSwapper is Ownable {
 
     // exchanged_token - адрес токена, на который мы меняем эфиры
     function swapExactETHForTokens(address exchanged_token) payable external {
+        require(address(this).balance != 0, "contract balance is zero");
         address[] memory path = new address[](2);
         path[0] = uniswapV2Router02.WETH();
         path[1] = exchanged_token;
@@ -40,25 +42,26 @@ contract ContractSwapper is Ownable {
         // 2) массив - что мы обмениваем и на что
         // 3) на какой адрес начислять токены
         // 4) дедлайн - время после которого операция будет отменена
-        uniswapV2Router02.swapExactETHForTokens{value: address(this).balance}(1, path, address(this), currentTimestamp + 1800);
+        uniswapV2Router02.swapExactETHForTokens{value: msg.value}(1, path, address(this), currentTimestamp + 1800);
     }
 
 
     function receiveETH() public payable {
-        // на балансе c отправляемого эфир аккаунта должно быть достаточно эфиров
-        require(msg.sender.balance >= msg.value, "Account has not enough eth to send");
         payable (address(this)).transfer(msg.value);
     }
 
 
-    function withdrawToken(address _tokenContract, uint256 _amount) external onlyOwner {
+    receive() external payable {
+    }
+
+
+    function withdrawToken(address _tokenContract, address _to, uint256 _amount) external onlyOwner {
         IERC20 tokenContract = IERC20(_tokenContract);
-        tokenContract.safeTransfer(msg.sender, _amount);
+        tokenContract.safeTransfer(_to, _amount);
     }
 
 
     function withdrawETH(address payable _to) public onlyOwner {
-        _to.transfer(address(this).balance);
+        payable(_to).transfer(address(this).balance);
     }
-
 }
